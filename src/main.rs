bc9e58383f01327e2a97a424e3aad9312e4290eb
ReubenHillyard <reuben.hillyard@outlook.com>
2023-06-29 17:54:30 +0100

    Added modules, dependencies, and doc-comment.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! A dependently-typed programming language, aiming to support safe mutable state and a cubical
+//! interpretation of univalence.
+
+mod ir_gen;
+mod lexer;
+mod parser;
+mod typing;
+
 fn main() {
     println!("Hello, world!");
 }
